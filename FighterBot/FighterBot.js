9ec364bb73ee29301bde2dd01475080f8c43034c
Sheepy3 <sheepy680@gmail.com>
2025-07-17 12:27:15 -0400

    moves, attacks, resets

--- FighterBot/FighterBot.js
+++ FighterBot/FighterBot.js
@@ -5,6 +5,7 @@ const armorManager = require('mineflayer-armor-manager')
 
 //const Vec3 = require('vec3')
 
+//BOT INSTANCE
 const bot = mineflayer.createBot({
   host:'173.73.200.194',//host: '173.73.200.194',
   port: 25565,
@@ -13,16 +14,86 @@ const bot = mineflayer.createBot({
   auth: 'offline'
 });
 
-//
+//VARIABLES & CONSTANTS
 var state ="idle"
 var target = null
-const targetting_range = 10
-const kited_range = 50
-const reach = 3
+const TARGETING_RANGE = 10
+const KITE_RANGE = 50
+const REACH = 3
+const CPS = 3
+const COOLDOWN = new Map()
+const LASTACTION = new Map()
 
 bot.loadPlugin(pathfinder)
 bot.loadPlugin(armorManager)
 
+
+/*bot state priority
+1. heal
+2. eat food
+3. equip gear 
+4. attack target
+5. move to target
+6. get new target
+*/
+
+bot.on("death", () => {
+    bot_reset()
+});
+
+
+bot.on('physicsTick', async () => {
+    //bot decision tree is an if else loop, meaning it can only be in one state at a time. 
+    if(target == null){
+        get_new_target()
+    }
+    else if (bot.entity.position.distanceTo(potential_target.position) > REACH){
+        if(bot.entity.position.distanceTo(potential_target.position) > KITE_RANGE){
+            console.log("kited")
+            bot_reset()
+            return
+        }
+        move_to_target()
+    }
+
+    else{
+        attack_target()
+    }
+    console.log(state)
+    //console.log(bot.getControlState('sprint'))
+});
+
+function bot_reset(){
+    bot.setControlState('sprint', false)
+    target = null
+    bot.pathfinder.setGoal(null)
+    console.log("RESETTING")
+
+}
+
+function get_new_target(){
+    state = "LOOKING FOR TARGET"
+    //console.log("getting new target")
+    potential_target = get_nearest_player()
+    if (potential_target && 
+        bot.entity.position.distanceTo(potential_target.position) < TARGETING_RANGE){
+            target = potential_target  
+        } 
+}
+
+function move_to_target(){
+    state = "MOVING TO TARGET"
+    bot.setControlState('sprint', true)
+    bot.pathfinder.setGoal(new goals.GoalFollow(target, 1))
+}
+
+function attack_target(){
+    state = "ATTACKING TARGET"
+    bot.lookAt(target.position)
+    bot.attack(target)
+}
+
+//helper functions
 function get_nearest_player(){
     const nearest = bot.nearestEntity(entity =>
         entity.type === 'player' &&
@@ -32,50 +103,3 @@ function get_nearest_player(){
     return nearest
 }
 
-
-/*bot state priority
-1. heal
-2. eat food
-3. equip gear 
-4. attack target
-5. move to target
-6. get to target
-
-*/
-bot.on('physicsTick', async () => {
-    
-    if(target == null){
-        get_new_target()
-    }
-    else if (bot.entity.position.distanceTo(potential_target.position) > reach &&
-            bot.entity.position.distanceTo(potential_target.position) < kited_range
-    ) {
-    move_to_target();
-    }
-    else{
-        attack_target()
-    }
-});
-
-function attack_target(){
-    console.log("hya!")
-}
-
-
-function move_to_target(){
-    bot.setControlState('sprint', true)
-    bot.pathfinder.setGoal(new goals.GoalFollow(target, 1))
-    console.log("hi") 
-}
-
-
-function get_new_target(){
-    //console.log("getting new target")
-    potential_target = get_nearest_player()
-    if (potential_target && 
-        bot.entity.position.distanceTo(potential_target.position) < targetting_range){
-            target = potential_target
-        } 
-}
-
-//function 
